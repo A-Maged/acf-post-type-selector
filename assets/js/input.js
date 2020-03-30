1a064b48bc7154038f401c2ae54379367ad5ff34
A-Maged <26769761+A-Maged@users.noreply.github.com>
2020-03-30 19:52:58 +0200

    improved select2 choosing none

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,63 +1,88 @@
 (function($) {
-	/**
-	 *  initialize_field
-	 *
-	 *  This function will initialize the $field.
-	 */
+    /**
+     *  initialize_field
+     *
+     *  This function will initialize the $field.
+     */
 
-	function initialize_field($field) {
-		//$field.doStuff();
+    function initialize_field($field) {
+        //$field.doStuff();
 
-		$selectElement = $field.find('select');
+        $selectElement = $field.find('select');
 
-		$selectElement.select2({
-			placeholder: 'Select Which Post types to show sidebar on',
-			closeOnSelect: false,
-			width: '100%'
-		});
+        /* initialize Select2 */
+        $selectElement.select2({
+            placeholder: 'Select Post types',
+            closeOnSelect: false,
+            width: '100%'
+        });
 
-		$field.on('change', function(e) {
-			// if is empty
-			if ($field.find('ul li').length < 2) {
-				// select null
-				$selectElement.val('null');
-			}
-		});
-	}
+        $selectElement.on('select2:select', function(e) {
+            var data = e.params.data;
 
-	if (typeof acf.add_action !== 'undefined') {
-		/*
-		 *  ready & append (ACF5)
-		 *
-		 *  These two events are called when a field element is ready for initizliation.
-		 *  - ready: on page load similar to $(document).ready()
-		 *  - append: on new DOM elements appended via repeater field or other AJAX calls
-		 *
-		 *  @param	n/a
-		 *  @return	n/a
-		 */
+            if (data.text === 'none') {
+                /* clear all */
+                $selectElement
+                    .val(null)
+                    .val('null')
+                    .trigger('change');
+            } else {
+                /* remove "null" option */
+                $selectElement
+                    .find('option[value="null"]')
+                    .prop('selected', false)
+                    .trigger('change');
+            }
+        });
 
-		acf.add_action('ready_field/type=cm_post_type_selector', initialize_field);
-		acf.add_action('append_field/type=cm_post_type_selector', initialize_field);
-	} else {
-		/*
-		 *  acf/setup_fields (ACF4)
-		 *
-		 *  These single event is called when a field element is ready for initizliation.
-		 *
-		 *  @param	event		an event object. This can be ignored
-		 *  @param	element		An element which contains the new HTML
-		 *  @return	n/a
-		 */
+        $selectElement.on('select2:unselect', function(e) {
+            /* if no elements are selected */
+            if ($selectElement.select2('data')) {
+                /* choose "null" option */
+                $selectElement.val('null').trigger('change');
+            }
+        });
+    }
 
-		$(document).on('acf/setup_fields', function(e, postbox) {
-			// find all relevant fields
-			$(postbox)
-				.find('.field[data-field_type="cm_post_type_selector"]')
-				.each(function() {
-					// initialize
-					initialize_field($(this));
-				});
-		});
-	}
+    if (typeof acf.add_action !== 'undefined') {
+        /*
+         *  ready & append (ACF5)
+         *
+         *  These two events are called when a field element is ready for initizliation.
+         *  - ready: on page load similar to $(document).ready()
+         *  - append: on new DOM elements appended via repeater field or other AJAX calls
+         *
+         *  @param	n/a
+         *  @return	n/a
+         */
+
+        acf.add_action(
+            'ready_field/type=acf_post_type_selector',
+            initialize_field
+        );
+        acf.add_action(
+            'append_field/type=acf_post_type_selector',
+            initialize_field
+        );
+    } else {
+        /*
+         *  acf/setup_fields (ACF4)
+         *
+         *  These single event is called when a field element is ready for initizliation.
+         *
+         *  @param	event		an event object. This can be ignored
+         *  @param	element		An element which contains the new HTML
+         *  @return	n/a
+         */
+
+        $(document).on('acf/setup_fields', function(e, postbox) {
+            // find all relevant fields
+            $(postbox)
+                .find('.field[data-field_type="acf_post_type_selector"]')
+                .each(function() {
+                    // initialize
+                    initialize_field($(this));
+                });
+        });
+    }
 })(jQuery);
